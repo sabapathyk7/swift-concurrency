522969d066a971f107ab719dfef2ff67757dfaef
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-31 13:27:30 +0530

    adds asyncawait

--- SwiftConcurrency.playground/Pages/AsyncAwait.xcplaygroundpage/Contents.swift
+++ SwiftConcurrency.playground/Pages/AsyncAwait.xcplaygroundpage/Contents.swift
@@ -2,6 +2,15 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+// Define a function that simulates an asynchronous task
+func performTask(taskNumber: Int) async {
+    print("Task \(taskNumber) started.")
+    do {
+        try await Task.sleep(nanoseconds: 1_000_000_000) // Simulate some asynchronous work (1 second)
+        print("Task \(taskNumber) completed.")
+    } catch {
+        print(error)
+    }
+}
+
